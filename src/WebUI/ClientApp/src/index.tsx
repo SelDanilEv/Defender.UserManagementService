4c99cb86803b388786967148068d8c79aa159663
Danil <47692381+SelDanilEv@users.noreply.github.com>
2022-07-28 05:11:36 +0400

    Sync

--- src/WebUI/ClientApp/src/index.tsx
+++ src/WebUI/ClientApp/src/index.tsx
@@ -8,7 +8,8 @@ import 'nprogress/nprogress.css';
 import App from 'src/App';
 import { SidebarProvider } from 'src/contexts/SidebarContext';
 import store from "./state/store";
-import * as serviceWorker from 'src/serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 import config from './config.json';
 
 ReactDOM.render(
@@ -26,4 +27,13 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-serviceWorker.unregister();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
