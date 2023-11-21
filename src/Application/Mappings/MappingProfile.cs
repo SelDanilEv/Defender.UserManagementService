50ce01fe81079b4d74a1ac12cb414cbdf166193c
Danil <47692381+SelDanilEv@users.noreply.github.com>
2023-11-21 19:58:48 +0100

    Update package version

--- src/Application/Mappings/MappingProfile.cs
+++ src/Application/Mappings/MappingProfile.cs
@@ -1,10 +1,10 @@
-using AutoMapper;
 using Defender.Common.DTOs;
+using Defender.Common.Mapping;
 using Defender.UserManagementService.Domain.Entities;
 
 namespace Defender.UserManagementService.Application.Common.Mappings;
 
-public class MappingProfile : Profile
+public class MappingProfile : BaseMappingProfile
 {
     public MappingProfile()
     {
